b458bf46f426adc24c59a2a117618cc9f08634b5
viki123v <viktorhristovski629@gmail.com>
2025-07-28 20:17:02 +0200

    Configure tailwind

--- buildboard-frontend-react/src/router.tsx
+++ buildboard-frontend-react/src/router.tsx
@@ -1,17 +1,26 @@
-import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+} from "react-router-dom";
+import HomePage from "@/pages/HomePage.tsx";
 
 export const router = createBrowserRouter(
-    createRoutesFromElements(
-        <>
-            <Route path={"/"}>{/*    tuka vo element ke napreme eden layout element za consistent ui*/}
-                <Route path={"topics/:id"}></Route>
-                <Route path={"projects/:id/"}>
-                    <Route path={"channels/:channelName"}></Route>
-                    <Route path={"requests"}></Route>
-                </Route>
-                <Route path={":username/profile"}></Route>
-                <Route path={"/about"}></Route>
-            </Route>
-        </>
-    )
-)
+	createRoutesFromElements(
+		<>
+			<Route
+				path={"/"}
+				element={<HomePage />}
+			>
+				{/*    tuka vo element ke napreme eden layout element za consistent ui*/}
+				<Route path={"topics/:id"}></Route>
+				<Route path={"projects/:id/"}>
+					<Route path={"channels/:channelName"}></Route>
+					<Route path={"requests"}></Route>
+				</Route>
+				<Route path={":username/profile"}></Route>
+				<Route path={"/about"}></Route>
+			</Route>
+		</>,
+	),
+);
