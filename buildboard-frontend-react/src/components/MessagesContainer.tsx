beb00a57196bc820a776e318a579a8379cc69235
stefan toskovski <stefantoska84@gmail.com>
2025-08-03 12:16:42 +0200

    feat: implement message sending, message container scrolling

--- buildboard-frontend-react/src/components/MessagesContainer.tsx
+++ buildboard-frontend-react/src/components/MessagesContainer.tsx
@@ -1,8 +1,11 @@
 import {Message} from "@/components/Message.tsx";
+import {type Ref, useEffect, useRef} from "react";
 
 export const MessagesContainer = ({messages}: {
-    messages: { user: string, content: string, date: string, userImage: string }[]
+    messages: { user: string, content: string, date: string, userImage: string }[],
 }) => {
+
+
     return (
         <div className={"flex flex-col gap-2 justify-start items-start"}>
             {messages.map((m, idx) => (
--- buildboard-frontend-react/src/pages/ChannelPage.tsx
+++ buildboard-frontend-react/src/pages/ChannelPage.tsx
@@ -1,18 +1,62 @@
-import { MessagesContainer } from "@/components/MessagesContainer.tsx"
+import {MessagesContainer} from "@/components/MessagesContainer.tsx"
 import {Card, CardContent, CardFooter, CardHeader} from "@/components/ui/card.tsx"
 import "../fonts.css"
 import {Textarea} from "@/components/ui/textarea.tsx";
-import {Button} from "@/components/ui/button.tsx";
+import * as React from "react";
+import {useEffect, useRef, useState} from "react";
 
 const messageData = [
-    {user: "stevetosak", content: "\"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi ar", date: "2025/07/01",userImage:"../public/vite.svg"},
-    {user: "ivan", content: "message content herer as sa", date: "2025/07/01",userImage:"../public/vite.svg"},
-    {user: "peco", content: "message content herer as sa", date: "2025/07/01",userImage:"../public/vite.svg"},
-    {user: "trajan", content: "message content herer as sa", date: "2025/07/01",userImage:"../public/vite.svg"},
-    {user: "stevetosak", content: "messageasdasddasd ", date: "2025/07/01",userImage:"../public/vite.svg"},
+    {
+        user: "stevetosak",
+        content: "\"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi ar",
+        date: "2025/07/01",
+        userImage: "../public/vite.svg"
+    },
+    {user: "ivan", content: "message content herer as sa", date: "2025/07/01", userImage: "../public/vite.svg"},
+    {user: "peco", content: "message content herer as sa", date: "2025/07/01", userImage: "../public/vite.svg"},
+    {user: "trajan", content: "message content herer as sa", date: "2025/07/01", userImage: "../public/vite.svg"},
+    {user: "stevetosak", content: "messageasdasddasd ", date: "2025/07/01", userImage: "../public/vite.svg"},
+    {user: "stevetosak", content: "messageasdasddasd ", date: "2025/07/01", userImage: "../public/vite.svg"},
+    {user: "stevetosak", content: "messageasdasddasd ", date: "2025/07/01", userImage: "../public/vite.svg"},
+    {user: "stevetosak", content: "messageasdasddasd ", date: "2025/07/01", userImage: "../public/vite.svg"},
+    {user: "stevetosak", content: "messageasdasddasd ", date: "2025/07/01", userImage: "../public/vite.svg"},
 ]
 
 export const ChannelPage = () => {
+
+    const [messages, setMessages] = useState<{
+        user: string,
+        content: string,
+        date: string,
+        userImage: string
+    }[]>(messageData)
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        const content = e.currentTarget.value;
+        if (e.key === "Enter" && !e.shiftKey && content.trim() !== '') {
+            e.preventDefault();
+            handleSendMessage(content)
+            e.currentTarget.value = ''
+        }
+    }
+
+    const handleSendMessage = (messageContent: string) => {
+        setMessages(prevMessages => [...prevMessages, {
+            user: "newMessageSender",
+            content: messageContent,
+            date: new Date().toDateString(),
+            userImage: ""
+        }])
+    }
+
+    const cardContainerRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if(cardContainerRef.current){
+            cardContainerRef.current.scrollTop = cardContainerRef.current.scrollHeight;
+        }
+    },[messages])
+
     return (
         <main className={"flex kanit-light border-none"}>
             <div className={"w-[300px] h-[600px]"}>
@@ -22,12 +66,15 @@ export const ChannelPage = () => {
                 <CardHeader className={"border-b-1 border-gray-800 mx-2 flex items-center"}>
                     <h1 className={"p-2"}>General</h1>
                 </CardHeader>
-                <CardContent>
-                    <MessagesContainer messages={messageData}></MessagesContainer>
-                </CardContent>
+                <div ref={cardContainerRef} className={"max-h-[50vh] overflow-y-auto"}>
+                    <CardContent>
+                        <MessagesContainer messages={messages}></MessagesContainer>
+                    </CardContent>
+                </div>
                 <CardFooter className="p-4 border-t border-gray-800 bg-background-main">
                     <div className="flex w-full items-center space-x-2">
                         <Textarea
+                            onKeyDown={handleKeyDown}
                             placeholder="Type your message..."
                             className="flex-1 resize-none rounded-xl border border-background-gray bg-background-card
                             text-white px-4 py-2 text-sm shadow-sm placeholder:text-foreground-gray
