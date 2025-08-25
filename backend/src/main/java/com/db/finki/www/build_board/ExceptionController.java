356fd27ef453d49b46d25434a2e30662ba8bf761
viki123v <viktorhristovski629@gmail.com>
2025-08-26 01:29:26 +0200

    fix: For internal problems return valid error json

--- backend/src/main/java/com/db/finki/www/build_board/ExceptionController.java
+++ backend/src/main/java/com/db/finki/www/build_board/ExceptionController.java
@@ -2,18 +2,36 @@ package com.db.finki.www.build_board;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.nimbusds.jose.JOSEException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.parsing.Problem;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice(annotations = RestController.class)
 public class ExceptionController {
+    private final HttpHeaders corsHeaders = new HttpHeaders(   MultiValueMap.fromSingleValue(
+            Map.of(
+                    "Access-Control-Allow-Origin", "http://localhost:5173",
+                    "Access-Control-Allow-Credentials", "true",
+                    "Access-Control-Allow-Headers", "Content-Type, Authorization",
+                    "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"
+                  )
+                                                                                               ));
+
     @ExceptionHandler(JOSEException.class)
     public ProblemDetail handleJOSEException(JOSEException e) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,"Sorry the server cannot verify that JOSE header");
@@ -44,5 +62,17 @@ public class ExceptionController {
         if(e.getMessage().contains("duplicate key"))
             return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,"The username already exists");
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,"Data integrity violation");
+    }
+
+    @ExceptionHandler(ServletException.class)
+    public ProblemDetail handleServletException(ServletException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ProblemDetail> handleAllExceptions(Exception ex) {
+        return new ResponseEntity<>(
+                ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage())
+                , corsHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
